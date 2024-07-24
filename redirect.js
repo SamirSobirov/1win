95779ca1d5278ed6cf11034e6303a6a26ecf3c70
Sobirov S <sobiroff23@gmail.com>
2024-07-24 12:41:38 +0500

    fixed

--- redirect.js
+++ redirect.js
@@ -1,15 +1,12 @@
-
 $(document).ready(function() {
     $('#loginButton').click(function() {
-        var email = $('input[type="email"]').val().toLowerCase()
-        var password = $('input[type="password"]').val().toLowerCase()
+        var email = $('input[type="email"]').val().toLowerCase();
+        var password = $('input[type="password"]').val().toLowerCase();
 
-        
         if (email === 'admin' && password === 'admin') {
-            window.location.href = "./index.html";
+            window.location.href = "./index.html"; // Переход на страницу index.html
         } else {
             alert('Неправильный логин или пароль. Попробуйте снова.');
         }
     });
 });
-
