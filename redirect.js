609eae5a704d93709d94f8d145d77539f7511353
Sobirov S <sobiroff23@gmail.com>
2024-07-24 11:27:23 +0500

    fixed

--- redirect.js
+++ redirect.js
@@ -4,9 +4,8 @@ $(document).ready(function() {
         var email = $('input[type="email"]').val();
         var password = $('input[type="password"]').val();
 
-        // Проверяем, если введены логин и пароль "admin"
+        
         if (email === 'admin' && password === 'admin') {
-            // Если верно, перенаправляем на index.html
             window.location.href = "./index.html";
         } else {
             alert('Неправильный логин или пароль. Попробуйте снова.');
