58d9f5530a7d8d7c78df5baee3c7b90212a7903b
Sobirov S <sobiroff23@gmail.com>
2024-07-24 10:35:54 +0500

    fixed

---
+++ login.js
@@ -0,0 +1,13 @@
+// window.onload = function() {
+//     if (window.location.pathname !== '/login.html') {
+//         window.location.href = "login.html";
+//     }
+
+//     const loginButton = document.getElementById('loginButton');
+//     if (loginButton) {
+//         loginButton.addEventListener('click', function(event) {
+//             event.preventDefault(); 
+//             window.location.href = "index.html";
+//         });
+//     }
+// };
--- redirect.js
+++ redirect.js
@@ -1,21 +1,16 @@
 window.onload = function() {
-    // Проверяем, на какой странице мы находимся при первой загрузке
-    if (window.location.pathname !== "/login.html" && window.location.pathname !== "/index.html") {
+    if (window.location.pathname !== "/login.html") {
         window.location.href = "login.html";
     }
+    onload()
 };
 
-// Устанавливаем обработчик события при загрузке DOM
 document.addEventListener("DOMContentLoaded", function() {
     const loginButton = document.getElementById('loginButton');
     if (loginButton) {
         loginButton.addEventListener('click', function(event) {
             event.preventDefault(); // Предотвращает переход по ссылке
-
-            // Переход на index.html, но только если мы на странице login.html
-            if (window.location.pathname === "/login.html") {
-                window.location.href = "index.html";
-            }
+            window.location.href = "index.html";
         });
     }
 });
